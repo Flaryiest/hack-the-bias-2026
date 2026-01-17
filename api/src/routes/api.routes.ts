9bf5bb00850f3832f591e9f7dd03e5459820fd41
jerryjiawu <68572439+jerryjiawu@users.noreply.github.com>
2026-01-17 11:57:58 -0700

    created python backend

--- api/src/routes/api.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const api = express.Router();
-
-api.get('/test', (req, res) => {
-  res.send('API is working properly');
-});
-
-export default api;
---
+++ backend/src/recognition.py
@@ -0,0 +1,1 @@
+#lamonkey do everything here
